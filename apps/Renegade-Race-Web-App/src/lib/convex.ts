f3674e2e6b2cdb0fad58af3000bbfbb55a6059a0
Malcolm Nunley <mxnunley1@gmail.com>
2025-10-06 23:15:08 -0400

    update 4

--- apps/Renegade-Race-Web-App/src/lib/convex.ts
+++ apps/Renegade-Race-Web-App/src/lib/convex.ts
@@ -1,5 +1,5 @@
+import { api } from '@renegade/backend';
 import { ConvexReactClient } from 'convex/react';
-import { api } from '../../../../packages/backend/convex/_generated/api';
 
 const convex = new ConvexReactClient(import.meta.env.VITE_CONVEX_URL as string);
 
--- apps/Renegade-Race-Web-App/vite.config.ts
+++ apps/Renegade-Race-Web-App/vite.config.ts
@@ -4,22 +4,7 @@ import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    {
-      name: 'replace-convex-server',
-      resolveId(id) {
-        if (id === 'convex/server') {
-          return id;
-        }
-      },
-      load(id) {
-        if (id === 'convex/server') {
-          return 'export {};';
-        }
-      },
-    },
-  ],
+  plugins: [react()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
